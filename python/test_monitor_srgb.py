0fe6c98d38d4531ad1e4441ddc75b42d20043fb9
Christian Pellegrin <chri@evolware.org>
2020-07-05 18:31:56 +0100

    Program to sample RGB color space.

--- python/test_monitor_srgb.py
+++ python/test_monitor_srgb.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
-import sys
+import argparse
+import csv
 import lcms2
 from collections import namedtuple
 import gi
 from typing import List
+import yasl.as726x as as726x
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk, GLib  # type: ignore # NOQA
+from gi.repository import Gtk  # type: ignore # NOQA
 
 
 Color = namedtuple("Color", "sR sG sB mR mG mB X Y Z x y")
@@ -16,10 +18,13 @@ def c8(c: int) -> float:
     return c/255.0
 
 
-def calc_colors(fname: str) -> List[Color]:
+def calc_colors(fname: str, step: int) -> List[Color]:
     srgb_profile = lcms2.cmsCreate_sRGBProfile()
     xyz_profile = lcms2.cmsCreateXYZProfile()
-    monitor_profile = lcms2.cmsOpenProfileFromFile(fname)
+    if fname == '':
+        monitor_profile = lcms2.cmsCreate_sRGBProfile()
+    else:
+        monitor_profile = lcms2.cmsOpenProfileFromFile(fname)
 
     srgb2monitor = lcms2.cmsCreateTransform(
         srgb_profile, lcms2.TYPE_RGB_8,
@@ -31,9 +36,10 @@ def calc_colors(fname: str) -> List[Color]:
         renderingIntent=lcms2.INTENT_RELATIVE_COLORIMETRIC)
 
     colors = []
-    for r in range(0, 257, 64):
-        for g in range(0, 257, 64):
-            for b in range(0, 257, 64):
+    endc = 254 + step
+    for r in range(0, endc, step):
+        for g in range(0, endc, step):
+            for b in range(0, endc, step):
                 if r > 255:
                     r = 255
                 if g > 255:
@@ -88,7 +94,7 @@ class ColorShow:
         cr.rectangle(0, 0, width, height)
         cr.fill()
         if self.cb:
-            self.cb()
+            self.cb(self.color)
 
     def set_color(self, c, cb=None):
         self.color = c
@@ -98,27 +104,107 @@ class ColorShow:
 
 class Sequencer:
 
-    def __init__(self, show, colors):
+    def __init__(self, show, colors, out, dev1=None, dev2=None):
         self.show = show
         self.colors = colors
-        self.i = 0
+        self.dev1 = dev1
+        self.dev2 = dev2
+        self.out = out
 
     def start(self):
-        GLib.timeout_add(200, self.next)
+        self.i = 0
+        self.res = []
+        self.next()
 
     def next(self):
-        self.show.set_color(self.colors[self.i])
-        self.i += 1
         if self.i >= len(self.colors):
-            self.i = 0
-        return True
+            self.finish()
+            return
+        self.show.set_color(self.colors[self.i], self.measure)
+        self.i += 1
+
+    def measure(self, color):
+        r = list(color)
+        # TODO: adapt integration time to lux?
+        if self.dev1 is not None:
+            r.append(self.dev1.get_temperature())
+            r.extend(self.dev1.measure())
+            r.append(self.dev1.get_lux())
+            r.append(self.dev1.get_cct())
+            r.extend(self.dev1.get_xy())
+            r.extend(self.dev1.get_uv())
+            r.append(self.dev1.get_duv())
+        else:
+            r.extend([0]*11)
+        if self.dev2 is not None:
+            r.append(self.dev2.get_temperature())
+            r.extend(self.dev2.measure())
+        else:
+            r.extend([0]*7)
+        self.res.append(r)
+        self.next()
+
+    def finish(self):
+        with open(self.out, 'w', newline='') as csvfile:
+            wr = csv.writer(csvfile)
+            for r in self.res:
+                wr.writerow(r)
+        Gtk.main_quit()
+
+
+def open_device(d, args):
+    dev = as726x.AS726x_SERIAL(d)
+    dev.set_bulb_current(0)
+    dev.set_indicator_current(0)
+    dev.set_bulb(0)
+    dev.set_indicator(0)
+    dev.set_gain(args.gain)
+    dev.set_integration(args.int_time)
+    return dev
 
 
 def main():
-    colors = calc_colors(sys.argv[1])
+    parser = argparse.ArgumentParser(description='sRGB monitor tester.')
+    parser.add_argument(
+        '--icc', dest='icc', action='store', default='',
+        help='ICC profile for the monitor')
+    parser.add_argument(
+        '--step', dest='step', action='store', default=64,
+        help='Sample step for the color space')
+    parser.add_argument(
+        '--deva', dest='deva', action='store', default='',
+        help='First device for acquisition')
+    parser.add_argument(
+        '--devb', dest='devb', action='store', default='',
+        help='Second device for acquisition')
+    parser.add_argument(
+        '--out', dest='out', action='store', default='out.csv',
+        help='CSV file with results')
+    parser.add_argument(
+        '--gain', dest='gain', action='store', default=0,
+        help='gain 0-3')
+    parser.add_argument(
+        '--int-time', dest='int_time', action='store', default=30,
+        help='integration time 1-255')
+    args = parser.parse_args()
+    dev1 = None
+    dev2 = None
+    if args.deva != '':
+        deva = open_device(args.deva, args)
+        if deva.get_version() == as726x.AS7261:
+            dev1 = deva
+        else:
+            dev2 = deva
+    if args.devb != '':
+        devb = open_device(args.deva, args)
+        if deva.get_version() == as726x.AS7261:
+            dev1 = devb
+        else:
+            dev2 = devb
+    colors = calc_colors(args.icc, args.step)
     show = ColorShow()
     show.create()
-    seq = Sequencer(show, colors)
+    seq = Sequencer(show, colors, args.out, dev1, dev2)
     seq.start()
     Gtk.main()
 
