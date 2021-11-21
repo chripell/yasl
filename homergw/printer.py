a0c52a664b0b2928322467caa96c5b33184176a3
Christian Pellegrin <chri@evolware.org>
2021-11-21 07:00:53 +0000

    Actually make printer selectable.

--- homergw/printer.py
+++ homergw/printer.py
@@ -8,16 +8,24 @@ class Impl(hrgw.Collector, hrgw.SleeperMixin):
 
     def __init__(self):
         self.running = True
+        self.active = False
 
     def register_args(self, arg: argparse.ArgumentParser):
         arg.add_argument("--printer", type=bool, default=False,
                          help="Enable printing of data.")
 
     async def start(self, args):
+        self.active = args.printer
+        if not self.active:
+            return
         print("Printer started.")
 
     async def push(self, p: hrgw.Data):
+        if not self.active:
+            return
         print(p.jd, p.name, p.data)
 
     async def stop(self):
+        if not self.active:
+            return
         print("Printer stopped.")
